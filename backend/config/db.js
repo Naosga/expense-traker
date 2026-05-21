219770c56ef70ece541a4470a7760d7b49feb1be
Nathalia Ospina Garcia <naosga98@gmail.com>
2026-05-21 22:14:17 +0300

    cambio para poder correr la app en linea

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-const { Pool } = require("pg");
+/*const { Pool } = require("pg");
 
 /*const pool = new Pool({
   user: "postgres",
@@ -10,7 +10,7 @@ const { Pool } = require("pg");
 
 module.exports = pool;*/
 
-require("dotenv").config();
+/*require("dotenv").config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -18,6 +18,19 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
+});
+
+module.exports = pool;*/
+
+
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 module.exports = pool;
