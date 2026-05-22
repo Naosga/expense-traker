445c3f6bd219055bd58a1f49f719e185e8eb4811
Nathalia Ospina Garcia <naosga98@gmail.com>
2026-05-22 21:43:23 +0300

    Temporarily hardcode production API URL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,15 @@
-import axios from "axios";
+/*import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+});
+
+export default api;*/
+
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://expense-traker-iakg.onrender.com/api",
 });
 
 export default api;
